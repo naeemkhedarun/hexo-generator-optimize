53e41df16796d648ee831aac0163b817963e3eb0
Naeem Khedarun <naeem.khedarun@gmail.com>
2015-12-29 17:36:37 +0000

    Remove dead code and reduce warnings.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var config = hexo.config.optimize;
-var async  = require('async')
-// hexo.public_dir
-// hexo.base_dir;
-// var desPathJs = hexo.public_dir+"js/final.js";
-// var desPathCss = hexo.public_dir+"css/finalcss.css";
-
 var gulp = require('gulp'),
 	gulpLoadPlugins = require('gulp-load-plugins'),
-	plugins = gulpLoadPlugins();
+	plugins = gulpLoadPlugins(),
+  async  = require('async');
+  
+var public_dir = hexo.public_dir;
+var config = hexo.config.optimize;
   
 // error function for plumber
 var onError = function (err) {
@@ -25,108 +22,49 @@ var AUTOPREFIXER_BROWSERS = [
   'bb >= 10'
 ];
 
-//build datestamp for cache busting
-var getStamp = function() {
-  var myDate = new Date();
-
-  var myYear = myDate.getFullYear().toString();
-  var myMonth = ('0' + (myDate.getMonth() + 1)).slice(-2);
-  var myDay = ('0' + myDate.getDate()).slice(-2);
-  var mySeconds = myDate.getSeconds().toString();
-
-  var myFullDate = myYear + myMonth + myDay + mySeconds;
-
-  return myFullDate;
-};
-
-// Optimize Images task
-gulp.task('images', function() {
-  return gulp.src(hexo.public_dir + '/images/*.{gif,jpg,png}')
-    .pipe(plugins.imagemin({
-        progressive: true,
-        interlaced: true,
-        svgoPlugins: [ {removeViewBox:false}, {removeUselessStrokeAndFill:false} ]
-    }))
-    .pipe(gulp.dest(hexo.public_dir + '/images/*.{gif,jpg,png}'))
-});
-
 gulp.task('css', function() {
-  return gulp.src(hexo.public_dir + '/css/*.css')
+  return gulp.src(public_dir + '/css/*.css')
     .pipe(plugins.plumber({ errorHandler: onError }))
     .pipe(plugins.concat('all.css'))
     .pipe(plugins.uncss({
-        html: [hexo.public_dir + '/index.html']
+        html: [public_dir + '/index.html']
     }))
     .pipe(plugins.autoprefixer(AUTOPREFIXER_BROWSERS))
     .pipe(plugins.base64({ extensions:['svg'] }))
     .pipe(plugins.rename({ suffix: '.min' }))
     .pipe(plugins.rev())
     .pipe(plugins.minifyCss())
-    .pipe(gulp.dest(hexo.public_dir + config.css_filepath))
+    .pipe(gulp.dest(public_dir + config.css_filepath))
     .pipe(plugins.rev.manifest())
-    .pipe(gulp.dest(hexo.public_dir + '/css/'))
+    .pipe(gulp.dest(public_dir + '/css/'))
     .pipe(plugins.notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('js', function() {
-  return gulp.src(hexo.public_dir + '/js/*.js')
+  return gulp.src(public_dir + '/js/*.js')
     .pipe(plugins.plumber({ errorHandler: onError }))
     .pipe(plugins.concat('all.js'))
     .pipe(plugins.rename({ suffix: '.min' }))
     .pipe(plugins.uglify())
     .pipe(plugins.rev())
-    .pipe(gulp.dest(hexo.public_dir + config.js_filepath))
+    .pipe(gulp.dest(public_dir + config.js_filepath))
     .pipe(plugins.rev.manifest())
-    .pipe(gulp.dest(hexo.public_dir + '/js/'))
+    .pipe(gulp.dest(public_dir + '/js/'))
     .pipe(plugins.notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('html', ["css", "js"],function() {
-  var cssManifest = gulp.src(hexo.public_dir + 'css/rev-manifest.json');
-  var jsManifest = gulp.src(hexo.public_dir + 'js/rev-manifest.json');
-  return gulp.src(hexo.public_dir + '/**/*.html')
+  var cssManifest = gulp.src(public_dir + 'css/rev-manifest.json');
+  var jsManifest = gulp.src(public_dir + 'js/rev-manifest.json');
+  return gulp.src(public_dir + '/**/*.html')
     .pipe(plugins.htmlReplace({css: config.css_webpath + 'all.min.css', js: config.js_webpath + 'all.min.js'}))
     .pipe(plugins.revReplace({manifest: cssManifest}))
     .pipe(plugins.revReplace({manifest: jsManifest}))
     .pipe(plugins.htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(hexo.public_dir));
+    .pipe(gulp.dest(public_dir));
 });
 
-// // Lint JS task
-// gulp.task('jslint', function() {
-//   return gulp.src('./public_html/assets/js/modules/*.js')
-//     .pipe(plugins.jshint())
-//     .pipe(plugins.jshint.reporter('default'))
-//     .pipe(plugins.jshint.reporter('fail'))
-//     .pipe(plugins.notify({ message: 'Lint task complete' }));
-// });
-
-// //Concatenate and Minify JS task
-// gulp.task('scripts', function() {
-//   return gulp.src('./public_html/assets/js/modules/*.js')
-//     .pipe(concat('webstoemp.js'))
-//     .pipe(gulp.dest('./public_html/assets/js/build'))
-//     .pipe(plugins.rename('webstoemp.min.js'))
-//     .pipe(plugins.stripdebug())
-//     .pipe(plugins.uglify())
-//     .pipe(gulp.dest('./public_html/assets/js/build'))
-//     .pipe(plugins.notify({ message: 'Scripts task complete' }));
-// });
-
-// // Cache busting task
-// gulp.task('cachebust', function() {
-//   return gulp.src('./craft/templates/_layouts/*.html')
-//     .pipe(plugins.replace(/screen.min.css\?([0-9]*)/g, 'screen.min.css?' + getStamp()))
-//     .pipe(plugins.replace(/print.min.css\?([0-9]*)/g, 'print.min.css?' + getStamp()))
-//     .pipe(plugins.replace(/webstoemp.min.js\?([0-9]*)/g, 'webstoemp.min.js?' + getStamp()))
-//     .pipe(gulp.dest('./craft/templates/_layouts/'))
-//     .pipe(plugins.notify({ message: 'CSS/JS Cachebust task complete' }));
-// });
-
-//tasks
-// gulp.task('default', ['css', 'jslint', 'scripts', 'cachebust']);
 gulp.task('default', ['css', 'js','html']);
-// gulp.task('images', ['img']);
 
 var optimize = function(args) {
     async.series([
@@ -144,7 +82,7 @@ var optimize = function(args) {
         },
     ], function(err){
         if (err) {
-            util.error("[error] Minify: -> " + err.message);
+            plugins.util.error("[error] Minify: -> " + err.message);
         }
     });
 };
